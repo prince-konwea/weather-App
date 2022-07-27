97aefef3e01ff20d89e64ba17646da67f018a7b9
kokodecontee <kokodecontee123@gmail.com>
2022-07-28 03:30:47 +1200

    create main.js file, declare variables for my cordinates and integrated fcc-weather-apibase url concatenated with my variable co-ordinates

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+    var lat;
+    var long;
+
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+
+            var api = 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat + '&lon='+ long + " ";
+        })
+    }
+});
